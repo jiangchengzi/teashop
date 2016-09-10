f3d54cc94e9b34a51941d2e47f76cb4e24cb1567
jiangchengzi <1025416045@qq.com>
2016-09-11 00:37:24 +0800

    熟悉一下views视图文件的编写,在views.py中加入了Login函数,看是否能够从数据库中读出数据并显示渲染到login.html中

--- login/views.py
+++ login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.template import loader,Context
+from django.http import HttpResponse
+from login.models import Users
 # Create your views here.
+
+def Login(request):
+    s=Users.objects.all()
+    t=loader.get_template('login.html')
+    c=Context({'shop':s})
+    return HttpResponse(t.render(c))
